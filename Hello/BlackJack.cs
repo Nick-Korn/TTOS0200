a402aa7cefbf725cdcb3c8ff30aa7877b3983d98
Niko Liimatainen <K8908@Labranet.jamk.fi>
2017-09-06 10:41:12 +0300

    Completetd refactor task

--- Hello/BlackJack.cs
+++ Hello/BlackJack.cs
@@ -10,26 +10,30 @@ namespace Hello {
             int myNumber = 17;
             int theirNumber;
             System.Console.WriteLine("*** BlackJack! ***");
-            System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-            //reading and converting 
-            theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-            //comparing that given umber is valid
-            if (theirNumber < 1 || theirNumber > 21)
+            do
             {
-                Console.WriteLine("The given number is out of limits, try again.");
-            }
-            else
-            {
-                //comparing
-                if (theirNumber >= myNumber && theirNumber <= 21)
+                System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                //reading and converting 
+                theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+                //comparing that given number is valid
+                if (theirNumber < 1 || theirNumber > 21)
                 {
-                    System.Console.WriteLine("You win.");
+                    Console.WriteLine("The given number is out of limits, try again.");
                 }
                 else
                 {
-                    System.Console.WriteLine("You lose.");
+                    //comparing
+                    if (theirNumber >= myNumber && theirNumber <= 21)
+                    {
+                        System.Console.WriteLine("You win.");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("You lose.");
+                    }
                 }
-            }
+            } while (theirNumber > 0 && theirNumber < 22);
+
         }
     }
 }
