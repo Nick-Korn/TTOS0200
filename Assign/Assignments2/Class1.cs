192a8d263d296a879c22e11725728bd83593d705
Liimatainen Niko <K8908@student.jamk.fi>
2017-09-18 22:35:10 +0300

    Finnished assignmnet 1 of lab 2

---
+++ Assign/Assignments2/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class SaunaHeater
+    {
+        private int heat = 0;
+        private int humidity = 0;
+
+        public int Heat
+        {
+            get
+            {
+                return heat;
+            }
+            set
+            {
+                heat = value;
+            }
+        }
+
+        public int Humidity
+        {
+            get
+            {
+                return humidity;
+            }
+            set
+            {
+                humidity = value;
+            }
+        }
+
+        public bool powerState { get; set; }
+
+        public void HeaterState()
+        {
+            Console.WriteLine("The state of the Sauna heater.");
+            if (powerState == false)
+            {
+                Console.WriteLine("Sauna is off!");
+            }
+            else if (powerState == true)
+            {
+                Console.WriteLine("Sauna is on!");
+                Console.WriteLine("Current temperature is {0} degreese celsius.", heat);
+                Console.WriteLine("Current humidity is {0}%", humidity);
+            }
+        }
+        
+        public void TurnOn()
+        {
+            powerState = true;
+        }
+
+        public void TurnOff()
+        {
+            powerState = false;
+        }
+
+        public void IncreaseHeat()
+        {
+            Heat += 5;
+        }
+        public void DecreaseHeat()
+        {
+            Heat -= 5;
+        }
+
+        public void IncreaseHumidity()
+        {
+            Humidity += 5;
+        }
+        public void DecreaseHumidity()
+        {
+            Humidity += 5;
+        }
+
+    }
+}
--- Assign/Assignments2/Program.cs
+++ Assign/Assignments2/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
-    class Assignments
+    class Program
     {
         static void Main(string[] args)
         {
+            Lab2.SaunaHeater harvia = new SaunaHeater();
+            int userInput;
+            harvia.HeaterState();
+            Console.WriteLine("Press enter to turn the sauna on!");
+            Console.ReadKey();
+            harvia.TurnOn();
+            do
+            {
+                Console.Clear();
+                harvia.HeaterState();
+                Console.WriteLine("\n\nPress 1 to increase heat \nPress 2 to decrease heat \nPress 3 to increase humidity. \nPress 4 to increase humidity\nPress 5 to shut the heater down!");
+                userInput = int.Parse(Console.ReadLine());
+                if (userInput == 1)
+                {
+                    harvia.IncreaseHeat();
+                }
+                else if (userInput == 2)
+                {
+                    harvia.DecreaseHeat();
+                }
+                else if (userInput == 3)
+                {
+                    harvia.IncreaseHumidity();
+                }
+                else if (userInput == 4)
+                {
+                    harvia.DecreaseHumidity();
+                }
+                else if (userInput == 5)
+                {
+                    harvia.TurnOff();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!!");
+                }
+
+            } while (harvia.powerState == true);
+
         }
     }
 }
