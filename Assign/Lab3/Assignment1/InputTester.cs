3f178603a0662168e37d40fa8f84f7368dd331ae
Liimatainen Niko <K8908@student.jamk.fi>
2017-09-26 16:51:09 +0300

    Added and finnished assignment1 of lab3

---
+++ Assign/Lab3/Assignment1/InputTester.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class InputTester
+    {
+        private char comma = ',';
+        private char slash = '/';
+        public bool IsItAInteger(string UserInput)
+        {
+            char[] inputArray = UserInput.ToCharArray();
+            bool parsedInput = double.TryParse(UserInput, out double number);
+
+            if (inputArray[0] == comma || inputArray[inputArray.Length - 1] == comma)
+            {
+                return false;
+            }
+            else
+            {
+                return parsedInput;
+            }
+            
+        }
+        public bool IsItADate(string UserInput)
+        {
+            CultureInfo culture;
+            DateTimeStyles styles;
+
+            // Settings for DateTime since my computers loclaization is in English format
+            culture = CultureInfo.CreateSpecificCulture("fi-FI");
+            styles = DateTimeStyles.None;
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+            bool parsedInput = DateTime.TryParse(UserInput, culture, styles, out DateTime dateValue);
+            char[] inputArray = UserInput.ToCharArray();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] == slash || inputArray[i] == comma)
+                {
+                    return false;
+                }
+            }
+            return parsedInput;
+        }
+    }
+}
---
+++ Assign/Lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+
+    class TestBed
+    {
+        static void Main(string[] args)
+        {
+            UseInputTester();
+        }
+
+        static void UseInputTester ()
+        {
+            string userInput;
+            Lab3.InputTester test = new InputTester();
+            while (true)
+            {
+                Console.WriteLine("Pleas input");
+                userInput = Console.ReadLine();
+                bool numberResult = test.IsItAInteger(userInput);
+                Console.WriteLine("Your input {0} is a number: {1}\n", userInput, numberResult);
+                bool dateResult = test.IsItADate(userInput);
+                Console.WriteLine("Your input {0} is a date: {1}\n", userInput, dateResult);
+            }
+
+        }
+    }
+}
