6befb4d4a836a8dfc086fd7e09cc5e24f55b2a3f
Niko Liimatainen <K8908@Labranet.jamk.fi>
2017-10-04 09:47:27 +0300

    Added Thing demo

---
+++ OOdemot/ThingCapsulation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOdemot
+{
+    class Thing
+    {
+        //public string Name;
+        //public string Name { get; set; }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Thing()
+        {
+            name = "Nobody here";
+        }
+
+        public override string ToString()
+        {
+            return string.Format("My name is {0}", Name);
+        }
+    }
+    
+}
