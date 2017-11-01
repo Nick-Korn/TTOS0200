6f4ffafc182707949db87ed5629e395062d716ad
Niko Liimatainen <K8908@Labranet.jamk.fi>
2017-11-01 09:52:50 +0200

    Updated week 44 demo

--- WK44/Program.cs
+++ WK44/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,13 @@ using System.Threading.Tasks;
 
 namespace JAMK.IT
 {
+    public struct Guider
+    {
+        public string Name;
+        public int Starts;
+        public int Wins;
+        public float WinPercentage;
+    }
     class Program
     {
         static void ReadGuidersVer1()
@@ -14,6 +22,48 @@ namespace JAMK.IT
             {
                 int counter = 0;
                 string line;
+                StreamReader stream = new StreamReader(@"D:\K8908\Data\tilastot2017.txt");
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine("{0} : {1}", counter, line);
+                    counter++;
+                }
+                stream.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        static void ReadGuidersVer2()
+        {
+            try
+            {
+                string seperator = ";";
+                string[] lines = File.ReadAllLines(@"D:\K8908\Data\tilasto2017.csv");
+                Guider driver;
+                int amount = lines.Length;
+                Console.WriteLine("Amount of guiders {0}", amount - 1);
+                for (int i = 1; i < amount; i++)
+                {
+                    string[] words = lines[i].Split(seperator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    if (int.TryParse(words[2], out int help))
+                    {
+                        driver.Name = words[0] + " " + words[1];
+                        driver.Starts = int.Parse(words[2]);
+                        driver.Wins = int.Parse(words[3]);
+                        driver.WinPercentage = (100f * driver.Wins / driver.Starts);
+                    }
+                    else
+                    {
+                        driver.Name = words[0] + " " + words[1] + " " + words[2];
+                        driver.Starts = int.Parse(words[3]);
+                        driver.Wins = int.Parse(words[4]);
+                        driver.WinPercentage = (100f * driver.Wins / driver.Starts);
+                    }
+                    Console.WriteLine("{0}: {1} - starts = {2}, wins = {3}, win percentage = {4}", i, driver.Name, driver.Starts, driver.Wins, driver.WinPercentage);
+                }
             }
             catch (Exception)
             {
@@ -25,7 +75,8 @@ namespace JAMK.IT
         {
             try
             {
-                ReadGuidersVer1();
+                //ReadGuidersVer1();
+                ReadGuidersVer2();
             }
             catch (Exception ex)
             {
