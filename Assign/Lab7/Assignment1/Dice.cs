eed6c10c61b1df11238050dbb32b8844e0d540ac
Liimatainen Niko <K8908@student.jamk.fi>
2017-11-06 18:37:38 +0200

    Finnished assignment 1 of assignmet set 7

---
+++ Assign/Lab7/Assignment1/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Dice
+    {
+        static Random DiceInt{ get; set; }
+        public int DiceThrow { get; set; }
+
+        static Dice()
+        {
+            DiceInt = new Random();
+        }
+        
+        public int ThrowDice()
+        {
+            int DiceThrow = DiceInt.Next(1, 7);
+            return DiceThrow;
+        }
+    }
+}
---
+++ Assign/Lab7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void ThrowDice()
+        {
+            int rollAmounts;
+            int roll;
+            List<int> numberList = new List<int>();
+            Dice test = new Dice();
+            Console.WriteLine("How many dice throws?");
+            rollAmounts = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rollAmounts; i++)
+            {
+                roll = test.ThrowDice();
+                numberList.Add(roll);
+            }
+            double avg = numberList.Average();
+            Console.WriteLine("The average of the throws is: {0}", avg);
+            for (int i = 1; i < 7; i++)
+            {
+                int x = 0;
+                foreach (int number in numberList)
+                {
+                    if (i == number)
+                    {
+                        x++;
+                    }
+                }
+                Console.WriteLine("The number {0} appears {1} times", i, x);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            ThrowDice();
+        }
+    }
+}
