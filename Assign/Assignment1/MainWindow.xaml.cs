66f1853cffe73faac4972aac8055f45060816a23
Liimatainen Niko <K8908@student.jamk.fi>
2017-11-13 20:41:00 +0200

    Finnished assignment 1 and 2 of assignment set 8

---
+++ Assign/Assignment1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int TruckCount { get; set; }
+        public int CarCount { get; set; }
+        public MainWindow()
+        {
+            TruckCount = 0;
+            CarCount = 0;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            truckTextBlock.Text = TruckCount.ToString();
+            TruckCount++;
+            truckTextBlock.Text = TruckCount.ToString();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            truckTextBlock.Text = TruckCount.ToString();
+            CarCount++;
+            carTextBlock.Text = CarCount.ToString();
+
+        }
+    }
+}
---
+++ Assign/Assignment2/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double ExchangeRate { get; set; }
+        public MainWindow()
+        {
+            ExchangeRate = 0.8997d;
+            InitializeComponent();
+        }
+        public void ConvertToEuro()
+        {
+
+            double amount = double.Parse(convTextBox.Text) * ExchangeRate;
+            convdTextBox.Text = amount.ToString("n2") + "€";
+        }
+        public void ConvertToDollar()
+        { 
+
+            double amount = double.Parse(convTextBox.Text) * ExchangeRate;
+            convdTextBox.Text = amount.ToString("n2") + "$";
+        }
+
+        private void convTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Double.TryParse(convTextBox.Text, out double result))
+            {
+                
+                if  (convdCoBox.SelectedIndex == 0)
+                {
+                    ConvertToDollar();
+                }
+                else if (convdCoBox.SelectedIndex == 1)
+                {
+                    ConvertToEuro();
+                }
+            }
+            else
+            {
+                convdTextBox.Text = "Invalid input"; 
+            }
+
+        }
+    }
+}
