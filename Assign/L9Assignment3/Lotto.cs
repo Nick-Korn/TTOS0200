75ecd3475fad2e02828e83bee1c6b14cd3a6aac0
Liimatainen Niko <K8908@student.jamk.fi>
2017-11-21 19:07:09 +0200

    Finnished assignment 3 of lab 9

---
+++ Assign/L9Assignment3/Lotto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9Assignment3
+{
+    class Lotto
+    {
+        public int AmountOfNumbers { get; set; }
+        public int NumberScale { get; set; }
+        public bool StarNumbers { get; set; }
+        static Random SingleNumber { get; set; }
+
+        public Lotto(int amountOfNumbers, int numberScale, bool starNumbers)
+        {
+            AmountOfNumbers = amountOfNumbers;
+            NumberScale = numberScale;
+            StarNumbers = starNumbers;
+
+        }
+
+        static Lotto()
+        {
+            // Static constructor for better randomazition
+            SingleNumber = new Random();
+        }
+
+        public string DrawLottoLine()
+        {
+            try
+            {
+                string line = "";
+                for (int i = 0; i < AmountOfNumbers; i++)
+                {
+                    int temp = SingleNumber.Next(1, NumberScale);
+                    line += temp + " ";
+                }
+                if (StarNumbers)
+                {
+                    line += SingleNumber.Next(1, 10) + " " + SingleNumber.Next(1, 10);
+                }
+                return line;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Assign/L9Assignment3/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L9Assignment3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> LottoLines { get; set; }
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+                LottoLines = new List<string>();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void drawBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int choice = gameCmbBox.SelectedIndex;
+                int amount = 0;
+                int scale = 0;
+                bool starNumbers = false;
+                resultTxtBox.Clear();
+                LottoLines.Clear();
+                switch (choice)
+                {
+                    case 0:
+                        amount = 7;
+                        scale = 40;
+                        starNumbers = false;
+                        break;
+                    case 1:
+                        amount = 6;
+                        scale = 48;
+                        starNumbers = false;
+                        break;
+                    case 2:
+                        amount = 5;
+                        scale = 50;
+                        starNumbers = true;
+                        break;
+                    default:
+                        MessageBox.Show("Invalid input for amount of draws");
+                        break;
+                }
+                Lotto draw = new Lotto(amount, scale, starNumbers);
+                string lottoLine;
+                for (int i = 0; i < int.Parse(drawTxtBox.Text); i++)
+                {
+                    lottoLine = "Line " + (i + 1) + ": " + draw.DrawLottoLine();
+                    LottoLines.Add(lottoLine);
+                }
+                foreach (var item in LottoLines)
+                {
+                    resultTxtBox.Text += item + "\n";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                resultTxtBox.Clear();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
